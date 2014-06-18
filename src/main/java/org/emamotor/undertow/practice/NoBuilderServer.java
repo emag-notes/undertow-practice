3d294a038759feddc0bf8af5776e484ea06a96fa
emag <lanabe.lanabe@gmail.com>
2014-06-18 17:32:21 +0900

    Add NoBuilderServer

---
+++ src/main/java/org/emamotor/undertow/practice/NoBuilderServer.java
@@ -0,0 +1,62 @@
+package org.emamotor.undertow.practice;
+
+import io.undertow.UndertowOptions;
+import io.undertow.server.protocol.http.HttpOpenListener;
+import org.xnio.BufferAllocator;
+import org.xnio.ByteBufferSlicePool;
+import org.xnio.OptionMap;
+import org.xnio.Options;
+import org.xnio.Pool;
+import org.xnio.Xnio;
+import org.xnio.XnioWorker;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ * @author tanabe
+ */
+public class NoBuilderServer {
+
+  private static final int IO_THREADS = 16;
+  private static final int WORKER_THREADS = 128;
+  private static final int BUFFER_SIZE = 512;
+  private static final int BUFFER_PER_REGION = 512;
+
+  public static void main(String[] args) {
+    Xnio xnio = Xnio.getInstance();
+
+    try {
+      XnioWorker worker = xnio.createWorker(OptionMap.builder()
+        .set(Options.WORKER_IO_THREADS, IO_THREADS)
+        .set(Options.WORKER_TASK_CORE_THREADS, WORKER_THREADS)
+        .set(Options.WORKER_TASK_MAX_THREADS, WORKER_THREADS)
+        .set(Options.TCP_NODELAY, true)
+        .getMap());
+
+      OptionMap socketOptions = OptionMap.builder()
+        .set(Options.WORKER_IO_THREADS, IO_THREADS)
+        .set(Options.TCP_NODELAY, true)
+        .set(Options.REUSE_ADDRESSES, true)
+        .getMap();
+
+      Pool<ByteBuffer> buffers = new ByteBufferSlicePool(
+        BufferAllocator.DIRECT_BYTE_BUFFER_ALLOCATOR,
+        BUFFER_SIZE,
+        BUFFER_SIZE * BUFFER_PER_REGION);
+
+      HttpOpenListener openListener = new HttpOpenListener(
+        buffers,
+        OptionMap.builder()
+          .set(UndertowOptions.BUFFER_PIPELINED_DATA, true)
+          .addAll(OptionMap.builder().getMap()) // serverOptions
+          .getMap(),
+        BUFFER_SIZE);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}

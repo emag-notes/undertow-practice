c96b55ec7af9ce9dc76fdf05fa4883a56b672009
emag <lanabe.lanabe@gmail.com>
2014-06-18 16:39:12 +0900

    Replace deprecated method

--- src/main/java/org/emamotor/undertow/practice/HelloWorldServer.java
+++ src/main/java/org/emamotor/undertow/practice/HelloWorldServer.java
@@ -13,7 +13,7 @@ public class HelloWorldServer {
     public static void main(String[] args) {
 
         Undertow server = Undertow.builder()
-                            .addListener(8080, "localhost")
+                            .addHttpListener(8080, "localhost")
                             .setHandler(new HttpHandler() {
                                 @Override
                                 public void handleRequest(final HttpServerExchange exchange) throws Exception {
